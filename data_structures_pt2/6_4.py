56cb1cf344cd143e52a63093649256b77a67c5fc
Clint Wickham <wickhcp@gmail.com>
2016-12-03 22:16:27 -0600

    Create 6_4.py

---
+++ data_structures_pt2/6_4.py
@@ -0,0 +1,9 @@
+'''Exercise 6.4
+There is a string method called count that is similar to the function
+in the previous exercise. Read the documentation of this method at docs.python.
+org/library/string.html and write an invocation that counts the number of
+times the letter a occurs in'banana'.'''
+
+word = 'banana'
+count = word.count('a',2,4)
+print(count)
