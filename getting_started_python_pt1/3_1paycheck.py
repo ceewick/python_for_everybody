bf5aa9b62ea26bedfd63fd2b4d3eb529337a9bf1
Clint Wickham <wickhcp@gmail.com>
2016-12-03 19:34:23 -0600

    3_1paycheck.py

---
+++ getting_started_python_pt1/3_1paycheck.py
@@ -0,0 +1,16 @@
+'''3.1 Write a program to prompt the user for hours and rate per hour using raw_input to compute gross pay. Award time-and-a-half for the hourly rate for all hours worked above 40 hours.
+
+Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75). You should use input to read a string and float() to convert the string to a number.
+
+Do not worry about error checking the user input - assume the user types numbers properly.'''
+
+input = raw_input('Hours?')
+hours = float(input)
+input = raw_input('rate?')
+rate = float(input)
+
+if hours > 40.0:
+    check = (rate * 40) + ((hours - 40) * (1.5 * rate))
+else:
+    check = rate * hours
+print (check)
