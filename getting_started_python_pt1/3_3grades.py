7f8fa69ea8e81f3e4a3969b7544eb3981e4b3fa0
Clint Wickham <wickhcp@gmail.com>
2016-12-03 20:22:47 -0600

    Update 3_3grades.py

--- getting_started_python_pt1/3_3grades.py
+++ getting_started_python_pt1/3_3grades.py
@@ -1,13 +1,15 @@
-#3.3 Write a program to prompt the user for a score using input.
-#Print out a letter grade based on the following table:
-#Score Grade
-#>= 0.9 A
-#>= 0.8 B
-#>= 0.7 C
-#>= 0.6 D
-#< 0.6 F
-#If the user enters a value out of range, print a suitable error message and
-#exit. For the test, enter a score of 0.85.
+'''3.3 Write a program to prompt the user for a score using input.
+Print out a letter grade based on the following table:
+Score Grade
+>= 0.9 A
+>= 0.8 B
+>= 0.7 C
+>= 0.6 D
+< 0.6 F
+If the user enters a value out of range, print a suitable error message and
+exit. For the test, enter a score of 0.85.'''
+
+##Disclaimer - this code not as efficient as could be
 
 try:
     inp = input('what score?')
@@ -24,5 +26,6 @@ try:
     elif 0.0 <= score <= 0.6:
         score = 'F'
     print(score)
+
 except:
     print('Error with input format OR code')
