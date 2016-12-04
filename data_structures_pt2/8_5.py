62fc7d3b10e3d3d666e53de6a298ca6799a34439
Clint Wickham <wickhcp@gmail.com>
2016-12-03 23:04:19 -0600

    Update 8_5.py

--- data_structures_pt2/8_5.py
+++ data_structures_pt2/8_5.py
@@ -1,22 +1,39 @@
-'''8.5 Open the file mbox-short.txt and read it line by line.
-When you find a line that starts with 'From ' like the following line:
-From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+'''Exercise 8.5
+Write a program to read through the mail box data and when you
+find line that starts with “From”, you will split the line into words using
+the split function. We are interested in who sent the message which is the
+second word on the From line.
 
-You will parse the From line using split() and print out the second
-word in the line (i.e. the entire address of the person who sent the
-message). Then print out a count at the end.
+From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008
+
+You will parse the From line and print out the second word for each From
+line and then you will also count the number of From (not From:) lines
+and print out a count at the end.
+
+This is a sample good output with a few lines removed:
+python fromcount.py
+Enter a file name: mbox-short.txt
+stephen.marquard@uct.ac.za
+louis@media.berkeley.edu
+zqian@umich.edu
+[...some output removed...]
+ray@media.berkeley.edu
+106
+Chapter 8. Lists
+cwen@iupui.edu
+cwen@iupui.edu
+cwen@iupui.edu
+There were 27 lines in the file with From as the first word
 
 Hint: make sure not to include the lines that start with 'From:'.'''
 
-
-file = open('mbox-short.txt', 'r')
-t = list()
+file = open('mbox-short.txt')
+s = list()
 count = 0
 
 for line in file:
-    line.rstrip()
     if line.startswith('From '):
-        count = count + 1
-        address = line.split()
-        print(address[1])  
-print(count)
+        email = line.split()
+        s.append(email[1])
+        count += 1
+print(s, count)
